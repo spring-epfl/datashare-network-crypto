c911228c646a27dddba08fa97542cdc00c6a71d2
kasra <kasra.edalat@gmail.com>
2020-06-18 16:55:34 +0200

    add init file

---
+++ mspsi/__init__.py
@@ -0,0 +1,13 @@
+"""
+Library implementing the multi-set PSI.
+"""
+
+__name__ = "mspsi"
+__package__ = __name__
+__author__ = "Kasra Edalatnejad, Laurent Girod"
+__copyright__ = "Copyright 2020, SPRING Lab, EPFL"
+__license__ = "BSD 3-Clause"
+__version__ = "0.9"
+
+from . import cpsi
+from . import mspsi
